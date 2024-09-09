6628b9e5188d52d4501eac62fcdcc8fce82398fd
unknown <ganijustine8@gmail.com>
2024-09-09 11:32:47 +0800

    initial commit

---
+++ number.js
@@ -0,0 +1,13 @@
+//Create a variable named firstFavNum and store your favorite number.
+let firstFavNum = 96;
+
+//Create a variable named secondFavNum and store your second favorite number.
+let secondFavNum = 69;
+
+//Perform the following operations.
+console.log("Addition: ", firstFavNum + secondFavNum);
+console.log("Subtraction: ", firstFavNum - secondFavNum);
+console.log("Multiplication: ", firstFavNum * secondFavNum);
+console.log("Division: ", firstFavNum / secondFavNum);
+console.log("Modulus: ", firstFavNum % secondFavNum);
+console.log("Exponentiation(Power): ", firstFavNum ** secondFavNum);
